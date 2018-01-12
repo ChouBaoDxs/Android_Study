007931b9fab4ac6ad30cc55b0cffb64eaf5370d8
董学森 <choubao@dongxuesendeMacBook-Air.local>
2018-01-12 15:54:27 +0800

    飞机小游戏

---
+++ 忘记来源了/Plane/README.md
@@ -0,0 +1,3 @@
+# Plane
+实现效果：飞机小游戏</br>
+![Image text](https://github.com/ChouBaoDxs/MyResources/blob/master/image/Android_Study/忘记来源了/plane.gif)
---
+++ 忘记来源了/Plane/app/src/main/java/com/choubao/www/plane/game/BulletAward.java
@@ -0,0 +1,14 @@
+package com.choubao.www.plane.game;
+
+import android.graphics.Bitmap;
+
+/**
+ * 子弹奖励
+ */
+public class BulletAward extends Award {
+
+    public BulletAward(Bitmap bitmap){
+        super(bitmap);
+    }
+
+}
