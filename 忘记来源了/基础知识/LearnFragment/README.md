7cdaad7eab04fd26829065fdf189d5d6c103c2f7
董学森 <choubao@dongxuesendeMacBook-Air.local>
2018-01-12 19:58:33 +0800

    LearnFragment

---
+++ 忘记来源了/基础知识/LearnFragment/README.md
@@ -0,0 +1,3 @@
+# LearnFragment
+实现效果：Fragment的基本使用</br>
+![Image text](https://github.com/ChouBaoDxs/MyResources/blob/master/image/Android_Study/忘记来源了/LearnFragment.png)
---
+++ 忘记来源了/基础知识/LearnFragment/app/src/main/java/com/example/choubao/learnfragment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.choubao.learnfragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container,new PlaceholderFragment()).commit();
+        }
+    }
+}
